75ba7a4cd0d79e3335847f3cea7bf086d73c7116
ihza <ihzahmahendra6661@gmail.com>
2025-10-15 11:12:30 +0700

    make nvim transparant

--- themes/catppuccin/neovim.lua
+++ themes/catppuccin/neovim.lua
@@ -1,5 +1,21 @@
 return {
 	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			transparent_background = true, -- ✅ enable transparency
+			flavour = "mocha", -- you can use: latte, frappe, macchiato, mocha
+			integrations = {
+				-- Optional integrations (uncomment as you like)
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = true,
+			},
+		},
+	},
+	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "catppuccin",
